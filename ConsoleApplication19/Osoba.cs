23e834ce3e47556984a335938eeab0a140a69eb7
pjastr <24464854+pjastr@users.noreply.github.com>
2017-10-16 12:20:25 +0200

    Add files via upload

---
+++ ConsoleApplication19/Osoba.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication19
+{
+    class Osoba
+    {
+        internal string imie;
+        public string nazwisko;
+        public int rokUrodzenia;
+        public int waga;
+        public int wzrost;
+        public bool okulary;
+        public string plec;
+
+        public int ObliczWiek()
+        {
+            return DateTime.Now.Year - rokUrodzenia;
+        }
+
+        public string Przedrostek()
+        {
+            if (plec == "M")
+                return "Pan";
+            else
+                return "Pani";
+        }
+        public double Bmi()
+        {
+            return waga / (wzrost * wzrost);
+
+            //if (Bmi() > 18.5) Console.WriteLine("Bmi prawidlowe");
+          
+        }
+        public void info2()
+        {
+            if (Bmi() > 18.5 && Bmi() < 24.99) Console.WriteLine("Bmi prawidlowe");
+            else Console.WriteLine("Bmi nieprawidlowe");
+        }
+    }
+}
---
+++ ConsoleApplication19/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Osoba dyrektor = new Osoba();
+            dyrektor.imie = "Jan";
+            dyrektor.nazwisko = "Kowalski";
+            dyrektor.rokUrodzenia = 1976;
+            dyrektor.plec = "M";
+            Console.WriteLine(dyrektor.Przedrostek() + " " + dyrektor.imie + " " + dyrektor.nazwisko + " " + dyrektor.ObliczWiek());
+            Osoba pacjent = new Osoba();
+            pacjent.imie = "Krzysztof";
+            pacjent.nazwisko = "Nowak";
+            pacjent.wzrost = 175;
+            pacjent.waga = 70;
+            pacjent.info2();
+            Console.ReadKey();
+        }
+    }
+}
